31984240ff873b24730e96953d4afdbeed363064
alexn <alexn3469@mail.ru>
2022-10-20 20:37:41 +0300

    Initial Init

---
+++ dispatcher/src/main/java/ru/alexn/DispatcherApplication.java
@@ -0,0 +1,13 @@
+package ru.alexn;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
+    public static void main(String[] args) {
+
+       SpringApplication.run(DispatcherApplication.class);
+    }
+}
---
+++ dispatcher/src/main/java/ru/alexn/controller/TelegramBot.java
@@ -0,0 +1,56 @@
+package ru.alexn.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@Component
+@Log4j2
+public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${hot.token}")
+    private String hotToken;
+
+
+    @Override
+    public String getBotUsername() {
+
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+
+        return hotToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        log.debug(originalMessage.getText());
+
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
+    }
+}
+
